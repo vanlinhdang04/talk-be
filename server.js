f80a49e7261ab18a57c9809ccda1be2b77748632
vanlinhdang04 <vanlinhdang1999@gmail.com>
2023-07-14 02:08:13 +0700

    add api test ci/cd

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
+app.get("/api", (req, res) => {
+  res.send("API is running.");
 });
 
 app.use("/api/user", userRoutes);
